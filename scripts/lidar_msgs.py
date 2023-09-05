bc9385ae562cbb734c6cddeadd9dc26f055f4b29
Varshith R V <vrv278@gmail.com>
2023-09-05 16:23:00 +0530

    1st iteration collision detection done

--- scripts/lidar_msgs.py
+++ scripts/lidar_msgs.py
@@ -1,11 +1,19 @@
 # to determine the nearest objects vector
 import rospy
-import sys
+import sys, threading
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Vector3
 import math
 
 scan=LaserScan()
+
+def ping_collision(i,body_field : LaserScan): # parse i*60 -> (i+1)*60
+    while not rospy.is_shutdown():
+        for j in range(i*60,(i+1)*60):
+            if scan.ranges[j] < body_field.ranges[j]:
+                rospy.logwarn(f"Collision detected in sector {i} with distance {scan.ranges[j]}")
+                break
+
 
 def callback(data: LaserScan):
     global scan
@@ -13,17 +21,12 @@ def callback(data: LaserScan):
 
 if __name__=="__main__":
     rospy.init_node("lidar_msgs")
-    
-    # get the rosparam tune
-    tune = float(rospy.get_param("/tune"))
+    rospy.loginfo("Initializing the collision_detection")
+    tune = float(sys.argv[1])
+
     rospy.Subscriber("/front/scan", LaserScan, callback)
     rate = rospy.Rate(20)
     rospy.wait_for_message("/front/scan",LaserScan)
-    
-    #scan information example
-    # print(len(scan.ranges), (scan.angle_max-scan.angle_min)/scan.angle_increment)
-    # print(type(scan.ranges))
-
 
     # define body field over the lidar, if the distance is less than r, then collision is iminent
     side = 0.42
@@ -31,6 +34,8 @@ if __name__=="__main__":
     effective_radius = (1 + tune/100)*r
     rospy.loginfo(f"Effective radius set to {effective_radius}")
 
+    ### body declaration
+    # ##############################     CURRENTLY THE BODY FIELD IS A CYLINDER, TO BE REPLACED BY A BOX ###########
     # create LaserScan message as body field
     body_field = LaserScan()
     body_field.header.frame_id = "base_link"
@@ -41,10 +46,8 @@ if __name__=="__main__":
     body_field.range_max = scan.range_max
     body_field.ranges = [effective_radius for i in range(len(scan.ranges))]
 
-    # while not rospy.is_shutdown():
-    #     # get for closest angle
-    #     # print(f"angel max :{scan.angle_max},angel min : {scan.angle_min},angle increment : {scan.angle_increment}")
-    #     # print(f"range_max : {scan.range_max}, range_min : {scan.range_min}")
-
-
-    #     rate.sleep()
+    # create 12 threads
+    threads = [threading.Thread(target=ping_collision, args=(i,body_field)) for i in range(12)]
+    for thread in threads:
+        thread.start()
+    
